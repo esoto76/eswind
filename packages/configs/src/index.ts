daeb85046c4ecc69aa4a9eabcdb14616f837d363
esoto76 <elpasocolorartist@yahoo.com>
2022-04-26 08:25:13 -0600

    feat(configs): add colors

--- packages/configs/src/index.ts
+++ packages/configs/src/index.ts
@@ -1,1 +1,11 @@
-export * from "./screens";
+import { colors } from "./colors";
+import { screens } from "./screens";
+
+export { screens, colors };
+export interface Configs {
+  screens: typeof screens;
+  colors: typeof colors;
+}
+
+const configs: Configs = { screens, colors };
+export default configs;
