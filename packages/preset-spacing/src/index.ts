e1e867ea22610dfb6fdde256a9ab37b94734545e
esoto76 <elpasocolorartist@yahoo.com>
2022-08-21 15:03:27 -0600

    feat(preset-spacing): add core exports

--- packages/preset-spacing/src/index.ts
+++ packages/preset-spacing/src/index.ts
@@ -1,1 +1,16 @@
-export * from './pkg'
+import { GetSpacing } from './fn';
+import { GetPresetFn, PresetConfig } from './types';
+
+export const GetPreset: GetPresetFn = options => {
+  return {
+    theme: {
+      spacing: GetSpacing(options || {})
+    }
+  };
+};
+
+export const Preset: PresetConfig = GetPreset();
+export { Preset as default };
+export * from './pkg';
+export * from './types';
+export { GetSpacing } from './fn';
