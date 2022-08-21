feb48f2438d34f7dcbbb65cd3331420603427db2
esoto76 <elpasocolorartist@yahoo.com>
2022-08-21 15:00:55 -0600

    feat(preset-spacing): add OKeys and MergeObjs

--- packages/preset-spacing/src/helpers.ts
+++ packages/preset-spacing/src/helpers.ts
@@ -12,3 +12,17 @@ export const IsArr: IsFn = v => (IsDef(v) && Array.isArray(v) ? true : false);
 
 export const IsObj: IsFn = v =>
   IsDef(v) && !IsArr(v) && typeof v === 'object' ? true : false;
+
+export const OKeys: (v: any) => string[] = v =>
+  IsObj(v) ? Object.keys(v) : [];
+
+export const MergeObjs: (v: { [key: string]: any }[]) => {
+  [key: string]: any;
+} = v => {
+  if (IsObj(v)) return v;
+  if (!IsArr(v) || v.length === 0) return {};
+  const obj = {};
+  for (let o of v) if (IsObj(o)) for (let k in o) obj[k] = o[k];
+
+  return obj;
+};
