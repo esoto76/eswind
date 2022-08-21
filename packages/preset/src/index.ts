d24141a921603553b31bd653fcf49c00e9be9cfe
esoto76 <elpasocolorartist@yahoo.com>
2022-08-21 17:51:36 -0600

    feat(preset): add @eswind/preset-screens

--- packages/preset/src/index.ts
+++ packages/preset/src/index.ts
@@ -1,9 +1,17 @@
 import { PresetConfig } from './types';
 import { SpacingConfig, GetSpacing } from '@eswind/preset-spacing';
+import { ScreensPreset, Screens } from '@eswind/preset-screens';
 
-export const Preset: Partial<PresetConfig> = { preset: [SpacingConfig] };
+export const Preset: Partial<PresetConfig> = {
+  preset: [SpacingConfig, ScreensPreset]
+};
 
 export * from './pkg';
 export * from './types';
 
-export { Preset as default, SpacingConfig as SpacingPreset, GetSpacing };
+export {
+  Preset as default,
+  SpacingConfig as SpacingPreset,
+  GetSpacing,
+  Screens
+};
