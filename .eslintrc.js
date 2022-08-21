68217dd0f640a35b10b8ee9606407eefb76b4348
esoto76 <elpasocolorartist@yahoo.com>
2022-08-21 15:05:54 -0600

    build: add trailing semicolor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
-const DOMGlobals = ['window', 'document']
-const NodeGlobals = ['module', 'require']
+const DOMGlobals = ["window", "document"];
+const NodeGlobals = ["module", "require"];
 
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { project: ['./tsconfig.json'] },
-  plugins: ['@typescript-eslint'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: ["./tsconfig.json"] },
+  plugins: ["@typescript-eslint"],
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   rules: {
-    semi: [2, 'always'],
-    'no-unused-vars': ['error'],
-    'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
+    semi: [1, "always"],
+    "no-unused-vars": ["error"],
+    "no-restricted-globals": ["error", ...DOMGlobals, ...NodeGlobals],
     ignoreParameters: true,
-    ignoreProperties: true
-  }
-}
+    ignoreProperties: true,
+  },
+};
